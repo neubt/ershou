01f492f7fb2abadffde43fb550d29b17d9432e67
wuwx <wuweixin@gmail.com>
2013-05-03 23:18:29 +0800

    二手市场导航栏高亮

--- app/controllers/ershou/application_controller.rb
+++ app/controllers/ershou/application_controller.rb
@@ -3,6 +3,12 @@ module Ershou
 
     before_filter :authenticate_user!
 
+    before_filter do    
+      @current = [ topics_path ]
+      drop_page_title("二手市场")
+      drop_breadcrumb("二手市场", topics_path)
+    end
+
     protected
       def current_ability
         @current_ability ||= Ability.new(current_user)
