65ee85e62b475fab659f9dce27e0ccaa4375ecd1
wuwx <wuweixin@gmail.com>
2013-05-06 10:52:01 +0800

    application controller

---
+++ app/controllers/ershou/settings/application_controller.rb
@@ -0,0 +1,6 @@
+require_dependency "ershou/application_controller"
+
+module Ershou
+  class Settings::ApplicationController < ApplicationController
+  end
+end
--- app/controllers/ershou/settings/topics_controller.rb
+++ app/controllers/ershou/settings/topics_controller.rb
@@ -1,6 +1,6 @@
 require_dependency "ershou/settings/application_controller"
 
 module Ershou
-  class Settings::TopicsController < ApplicationController
+  class Settings::TopicsController < Settings::ApplicationController
   end
 end
