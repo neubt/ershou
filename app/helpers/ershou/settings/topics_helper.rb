988642798bf86c798d4b422253e7078fc7cd08c8
wuwx <wuweixin@gmail.com>
2013-05-06 10:46:11 +0800

    settings/topics controller #15

---
+++ app/helpers/ershou/settings/topics_helper.rb
@@ -0,0 +1,4 @@
+module Ershou
+  module Settings::TopicsHelper
+  end
+end
---
+++ test/unit/helpers/ershou/settings/topics_helper_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module Ershou
+  class Settings::TopicsHelperTest < ActionView::TestCase
+  end
+end
