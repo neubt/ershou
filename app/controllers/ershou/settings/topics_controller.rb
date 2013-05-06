8fb9b015c7c4c6662d6471792f6d69bc77f9778b
wuwx <wuweixin@gmail.com>
2013-05-06 11:57:35 +0800

    my topics #15

--- app/controllers/ershou/settings/topics_controller.rb
+++ app/controllers/ershou/settings/topics_controller.rb
@@ -2,5 +2,10 @@ require_dependency "ershou/settings/application_controller"
 
 module Ershou
   class Settings::TopicsController < Settings::ApplicationController
+
+    def index
+      @topics = Topic.where(:user_id => current_user.id)
+    end
+    
   end
 end
