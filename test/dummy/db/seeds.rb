87cbec6bed9e69acabbb1399232e295bc6ed6b02
wuwx <wuweixin@gmail.com>
2013-05-08 09:45:48 +0800

    帖子seed

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -1,8 +1,21 @@
 # encoding: UTF-8
 
+User.create(:name => "user", :email => "user@neubt.com", :password => "user1234")
+
 module Ershou
+
   Node.create(:name => "node1")
   Node.create(:name => "node2")
   Node.create(:name => "node3")
   Node.create(:name => "node4")
+
+  100.times do |i|
+    Topic.new do |topic|
+      topic.title = "二手市场的第#{i}个帖子"
+      topic.content = "二手市场的第#{i}个帖子的内容"
+      topic.user_id = 1
+      topic.save
+    end
+  end
+
 end
