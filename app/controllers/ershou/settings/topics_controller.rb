7fd74bae9711ba06c2854a9ebb55b923d965726c
wuwx <wuweixin@gmail.com>
2013-05-10 16:54:44 +0800

    fixed #17

--- app/controllers/ershou/settings/topics_controller.rb
+++ app/controllers/ershou/settings/topics_controller.rb
@@ -4,7 +4,9 @@ module Ershou
   class Settings::TopicsController < Settings::ApplicationController
 
     def index
-      @topics = Topic.where(:user_id => current_user.id).page(params[:page]).reverse_order.decorate
+      @topics = Topic.where(:user_id => current_user.id)
+      @topics = @topics.where(:state => params[:state]) if ['open', 'closed'].include?(params[:state])
+      @topics = @topics.page(params[:page]).reverse_order.decorate
     end
     
   end
