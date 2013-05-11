385c768a65f9845b2588485177d0a142ee88c492
wuwx <wuweixin@gmail.com>
2013-05-11 15:00:01 +0800

    settings and admin require login

--- app/controllers/ershou/admin/application_controller.rb
+++ app/controllers/ershou/admin/application_controller.rb
@@ -2,5 +2,6 @@ require_dependency "ershou/application_controller"
 
 module Ershou
   class Admin::ApplicationController < ApplicationController
+    before_filter :authenticate_user!
   end
 end
--- app/controllers/ershou/settings/application_controller.rb
+++ app/controllers/ershou/settings/application_controller.rb
@@ -2,5 +2,6 @@ require_dependency "ershou/application_controller"
 
 module Ershou
   class Settings::ApplicationController < ApplicationController
+    before_filter :authenticate_user!
   end
 end
