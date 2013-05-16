4a9904052144cd26c712f78473f1f4afbf8bf148
wuwx <wuweixin@gmail.com>
2013-05-16 21:32:37 +0800

    asset bug

--- app/helpers/ershou/application_helper.rb
+++ app/helpers/ershou/application_helper.rb
@@ -6,5 +6,10 @@ module Ershou
     def markdown(text)
       Markdown.new(text, :autolink).to_html.html_safe
     end
+    
+    def image_tag(source, options={})
+      super(source, options) if source.present?
+    end
+    
   end
 end
